20243a67265cf2d441741a6602a2aeb3237c34d0
Himanshu <himanshu@costacloud.com>
2022-08-09 22:07:58 +0530

    changes

--- src/Components/Gridsystem.js
+++ src/Components/Gridsystem.js
@@ -114,12 +114,12 @@
 //               </div>
 //             </div>
 //           </Grid>
-          
+
 //           {open && (
 //             <>
-//             <Grid item xs={6}>
+//
 //             <TextField
-//               className={classes.input}
+//
 //               variant="outlined"
 //               fullWidth
 //               size="small"
@@ -129,7 +129,7 @@
 //               name="names"
 //             />
 //           </Grid>
-//               <Grid item xs={6}>
+//
 //                 <TextField
 //                   select
 //                   native="true"
@@ -270,43 +270,47 @@
 //           </GridComponent>
 //         </Grid>
 //         </Grid>
-       
+
 //       </Grid>
 //       )
 //     </div>
 //   );
 // };
 
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import Switch from '@material-ui/core/Switch';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
 import {
-     
-     Grid
-    
-     
-   
-   
-   } from "@material-ui/core";
-//import { Gridsystem } from "./Components/Gridsystem";
+  DialogTitle,
+  Grid,
+  IconButton,
+  MenuItem,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@material-ui/core";
+import "../Components/Gridsystem.css";
+import SearchIcon from "@material-ui/icons/Search";
+
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  GridComponent,
+  Inject,
+  Page,
+  Resize,
+} from "@syncfusion/ej2-react-grids";
+import FilterListIcon from "@material-ui/icons/FilterList";
 
 const useStyles = makeStyles((theme) => ({
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: 'auto',
-    width: 'fit-content',
+    display: "flex",
+    flexDirection: "column",
+    margin: "auto",
+    width: "fit-content",
   },
   formControl: {
     marginTop: theme.spacing(2),
@@ -321,7 +325,7 @@ export default function Gridsystem() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [fullWidth, setFullWidth] = React.useState(true);
-  const [maxWidth, setMaxWidth] = React.useState('sm');
+  const [maxWidth, setMaxWidth] = React.useState("sm");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -339,41 +343,201 @@ export default function Gridsystem() {
     setFullWidth(event.target.checked);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("working");
+  };
   return (
-    <React.Fragment>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open max-width dialog
-      </Button>
+    <Grid>
+      <Typography
+        variant="h4"
+        style={{ textAlign: "center", marginTop: "3rem", marginBottom: "1rem" }}
+      >
+        Advance Search Page
+      </Typography>
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <TextField
+            variant="outlined"
+            color="primary"
+            label="Content"
+            size="small"
+            fullWidth
+            SearchIcon
+            className="Date_field"
+            style={{ width: "700px", borderRadius: ".5rem" }}
+          />
+          <Tooltip title="Advance Search">
+            <FilterListIcon
+              style={{
+                position: "relative",
+                left: "-2rem",
+                top: ".5rem",
+                cursor: "pointer",
+              }}
+              onClick={handleClickOpen}
+            />
+          </Tooltip>
+          <Button type="submit" style={{ display: "none" }}></Button>
+        </form>
+      </div>
+
       <Dialog
         fullWidth={fullWidth}
         maxWidth={maxWidth}
         open={open}
         onClose={handleClose}
-        aria-labelledby="max-width-dialog-title"
+        style={{ marginTop: "6.5rem" }}
       >
-        <DialogTitle id="max-width-dialog-title">Optional sizes</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            You can set my maximum width and whether to adapt or not.
-          </DialogContentText>
-          <form className={classes.form} noValidate>
-        
-     <p>vhvhv</p>
+        <div style={{ padding: ".5rem", overflow: "hidden" }}>
+          <DialogTitle>
+            <Typography
+              variant="h5"
+              color="primary"
+              style={{ textAlign: "center" }}
+            >
+              Advance Search
+            </Typography>
+          </DialogTitle>
+          <form onSubmit={handleSubmit}>
+            <DialogContent>
+              <Grid style={{ paddingLeft: "-2rem" }}>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  label="UPLOADED BY:"
+                  // value={searchTerm.names}
+                  // onChange={handleInputChange}
+                  name="names"
+                  // style={{ marginTop: "1rem" }}
+                />
+
+                <TextField
+                  select
+                  native="true"
+                  className={classes.input}
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  label="TYPE:"
+                  style={{ marginTop: "1rem" }}
+                >
+                  <MenuItem value="">Choose one option</MenuItem>
+                  <MenuItem value="3">NOTING</MenuItem>
+                  <MenuItem value="6">ENCLOSURE</MenuItem>
+                  <MenuItem value="9">SERVICE LETTER</MenuItem>
+                  <MenuItem value="12">SERVICE NOTE</MenuItem>
+                  <MenuItem value="16">MINUTE OF METTING</MenuItem>
+                  <MenuItem value="18">DEMO OFFICIAL</MenuItem>
+                </TextField>
+
+                <TextField
+                  className={classes.input}
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  label="FILE NO:"
+                  style={{ marginTop: "1rem" }}
+                  // value={searchTerm.files}
+                  // onChange={handleInputChange}
+                  name="files"
+                />
+
+                <TextField
+                  className={classes.input}
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  label="FILE SUBJECT:"
+                  style={{ marginTop: "1rem" }}
+                />
+
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  label="OLD FILE REFERENCE:"
+                  style={{ marginTop: "1rem" }}
+                />
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  size="small"
+                  label="Date:"
+                  // value={searchTerm.search}
+                  // onChange={handleInputChange}
+                  // type="date"
+                  name="search"
+                  style={{ marginTop: "1rem" }}
+                />
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                color="primary"
+                variant="outlined"
+                startIcon={<SearchIcon />}
+                type="submit"
+              >
+                Search
+              </Button>
+            </DialogActions>
           </form>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Close
-          </Button>
-        </DialogActions>
+        </div>
       </Dialog>
-    </React.Fragment>
+      <GridComponent
+        // dataSource={product}
+        height="350"
+        allowResizing={true}
+        allowSorting={true}
+        allowPaging={true}
+        pageSettings={{ pageCount: 5, pageSizes: true }}
+        allowFiltering={true}
+        filterSettings={{ type: "Menu" }}
+        style={{ top: "2rem" }}
+      >
+        <ColumnsDirective>
+          <ColumnDirective
+            field="bucketId"
+            headerText={"UPLOADED BY"}
+            width="90"
+            textAlign="left"
+          />
+          <ColumnDirective field="data" width="200" headerText={"CONTENT"} />
+          <ColumnDirective
+            field="Doc subject"
+            headerText={"TYPE"}
+            width="150"
+            textAlign="center"
+          />
+          <ColumnDirective
+            field="fileName"
+            headerText={"FILE NO"}
+            width="130"
+            format="yMd"
+            textAlign="center"
+          />
+          <ColumnDirective
+            field="modified date"
+            headerText={"MODIFIED DATE"}
+            // template={statusTemplate}
+            width="120"
+            textAlign="Right"
+          />
+          <ColumnDirective
+            field="Date"
+            headerText={"CREATED DATE"}
+            width="130"
+            format="yMd"
+            textAlign="Right"
+          />
+        </ColumnsDirective>
+        <Inject services={[Resize, Page]} />
+      </GridComponent>
+    </Grid>
   );
 }
-    
-
-
-
-
-
-   
